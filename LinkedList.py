d68a42f3fbd3829a90130dcc388841edbedde6a7
Devesh Singh <dvsh.singh.796@gmail.com>
2020-11-17 23:17:59 +0530

    Added Tree implementation

--- LinkedList.py
+++ LinkedList.py
@@ -88,11 +88,15 @@ class LinkedList:
 
 if __name__ == '__main__':
     ll = LinkedList()
-    ll.insertData(["banana","mango","grapes","orange"])
-    ll.insert_at(1,"blueberry")
+    ll.insertData([45,7,12,567,99])
+    ll.print()
+    ll.insert_at(1,67)
+    ll.print()
+    ll.insert_at_end(86)
+    ll.print()
     ll.remove_at(2)
     ll.print()
 
-    # ll.insertData([45,7,12,567,99])
-    # # ll.insert_at_end(67)
+    # ll.insertData([])
+    # #
     # ll.print()
